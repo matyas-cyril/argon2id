0c47b640fb9fc37c0b6a577f273be3045ac808f7
matyas.cyril <matyas.cyril@gmail.com>
2026-01-21 10:20:32 +0100

    Déclaration des variables globales et structures

---
+++ define.go
@@ -0,0 +1,51 @@
+package argon2id
+
+import (
+	"fmt"
+	"runtime"
+
+	"golang.org/x/crypto/argon2"
+)
+
+type Params struct {
+	Memory    uint32 // m=
+	Iteration uint32 // t=
+	Parallel  uint8  // p=
+	SaltLen   uint8  // Longueur du salt si auto-generation
+	HashLen   uint32
+	Version   int
+	Argon2id  bool // type
+}
+
+type Argon2id struct {
+	params *Params
+	salt   []byte
+	hash   []byte
+}
+
+type ByteString interface {
+	[]byte | string
+}
+
+var DefaultParams = &Params{
+	Memory:    64,
+	Iteration: 1,
+	Parallel:  uint8(runtime.NumCPU()),
+	SaltLen:   16,
+	HashLen:   32,
+	Version:   argon2.Version,
+	Argon2id:  true,
+}
+
+func strToByte[T ByteString](input T) (data []byte, err error) {
+
+	switch p := any(input).(type) {
+	case string:
+		return []byte(p), nil
+	case []byte:
+		return []byte(p), nil
+	default:
+		return nil, fmt.Errorf("unsupported type")
+	}
+
+}
